719aa35a735773b256a39b0c0432a5a950d29d52
Roberto Santos <robot-o@hotmail.com>
2019-03-09 16:50:13 -0700

    update first solution

--- 6-kyu/6array_diff.js
+++ 6-kyu/6array_diff.js
@@ -9,27 +9,78 @@ array_diff([1,2,2,2,3],[2]) == [1,3]
 */
 
 function array_diff(a, b) {
+  let difference = [];
   if (a !== []) {
-    for (i=0; i<a.length; i++) {
-      for (j=0; j<b.length; j++) {
-        if (a[i] == b[j]) {
-          a.shift(a[i]);
+    a.forEach(function(current) {
+          if (current !== b[0]) {
+            difference.push(current);
         }
-      }
-    }
-  }
-  return a;
-}
-
-
-function array_diff(a,b) {
-  return a.filter((current !== b[0]), [])
-}
-
-function array_diff(a,b) {
-  for (i=0; i<a.length; i++) {
-    for (j=0; j<b.length; j++) {
-      a[i] !== b[j] ? difference.push(a[i]) : a[i] = ''
-    }
+      })
+      return difference;
+    } else {  
+    return a;
   }
 }
+
+// function array_diff(a, b) {
+//   if (a !== []) {
+//     for (i=0; i<a.length; i++) {
+//       for (j=0; j<b.length; j++) {
+//         if (a[i] == b[j]) {
+//           a.shift(a[i]);
+//         }
+//       }
+//     }
+//   }
+//   return a;
+// }
+
+// function array_diff(a, b) {
+//   let difference = [];
+//   if (a !== []) {
+//     a.forEach(function(current) {
+//         for (i=0; i<b.length; i++) {
+//           if (current !== b[i]) {
+//             difference.push(current);
+//           }
+//         }
+//       })
+//       return a;
+//     } else {  
+//     return difference;
+//   }
+// }
+
+
+// function array_diff(a,b) {
+//   let difference = [];
+//   if (b === []) {
+//     return difference;
+//     } else {
+//       a.forEach(function(current) {
+//         for (i=0; i<b.length; i++) {
+//           if (current !== b[i]) {
+//             difference.push(current);
+//           }
+//         }
+//       })
+//     }
+//   }
+
+
+
+// function array_diff(a,b) {
+//   let difference = a.filter(function(current) {
+//     for (i=0; i<b.length; i++) {
+//       return current !== b[i]
+//     }
+// })
+
+
+// function array_diff(a,b) {
+//   for (i=0; i<a.length; i++) {
+//     for (j=0; j<b.length; j++) {
+//       a[i] !== b[j] ? difference.push(a[i]) : a[i] = ''
+//     }
+//   }
+// }
